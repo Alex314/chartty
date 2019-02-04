e874d1aa36e69c6ab460c697fd3c3fc7a5fdfcf8
Alex314 <alex3.14mart@gmail.com>
2019-02-05 00:12:12 +0200

    remove example

--- flask_heroku_example/wsgi.py
+++
@@ -1,9 +0,0 @@
-from whitenoise import WhiteNoise
-
-from app import app, socketio
-
-# socketio.run(app)
-application = WhiteNoise(app)
-# application = WhiteNoise(socketio)
-application.add_files('static/', prefix='static/')
-# socketio.run(app)
